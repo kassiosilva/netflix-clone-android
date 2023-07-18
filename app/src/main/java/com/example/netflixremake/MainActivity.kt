67d902f2039f85ff4f09fca8ba2939c73f8d1a7d
Kássio Silva <Kassio.Sa@ibm.com>
2023-07-18 19:34:03 -0300

    feat: criando listagem simple

--- app/src/main/java/com/example/netflixremake/MainActivity.kt
+++ app/src/main/java/com/example/netflixremake/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.netflixremake
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = MainAdapter()
+        val recyclerMain: RecyclerView = findViewById(R.id.recycler_main)
+        recyclerMain.layoutManager = LinearLayoutManager(this)
+        recyclerMain.adapter = adapter
     }
 }
