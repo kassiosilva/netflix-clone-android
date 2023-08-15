7cf21f519da06d4a791d309326da52a88216401d
Kássio Silva <Kassio.Sa@ibm.com>
2023-08-15 14:31:32 -0300

    feat: Add callback para retornar os dados de listagem

--- app/src/main/java/com/example/netflixremake/MainActivity.kt
+++ app/src/main/java/com/example/netflixremake/MainActivity.kt
@@ -2,16 +2,24 @@ package com.example.netflixremake
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.netflixremake.model.Category
 import com.example.netflixremake.util.CategoryTask
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CategoryTask.Callback {
+
+    private lateinit var progress: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        progress = findViewById(R.id.progress_main)
 
         val categories = mutableListOf<Category>()
 
@@ -20,6 +28,20 @@ class MainActivity : AppCompatActivity() {
         recyclerMain.layoutManager = LinearLayoutManager(this)
         recyclerMain.adapter = adapter
 
-        CategoryTask().execute("https://api.tiagoaguiar.co/netflixapp/home?apiKey=f95c1bcb-03c1-48d3-b461-e38402a7ecb5")
+        CategoryTask(this).execute("https://api.tiagoaguiar.co/netflixapp/home?apiKey=f95c1bcb-03c1-48d3-b461-e38402a7ecb5")
+    }
+
+    override fun onPreExecute() {
+        progress.visibility = View.VISIBLE
+    }
+
+    override fun onResult(categories: List<Category>) {
+        Log.i("Teste MainActivity", categories.toString())
+        progress.visibility = View.GONE
+    }
+
+    override fun onFailure(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        progress.visibility = View.GONE
     }
 }
